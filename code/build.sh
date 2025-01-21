33717171fae8b48097c43412d0ddc1791405ffd6
terra <544708735@qq.com>
2025-01-21 09:08:25 +0800

    notes

--- code/build.sh
+++ code/build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-g++ -std=c++23 -O3 -g test.cpp -lspdlog -lgtest -lgmock -lssl -lcrypto -o test && ./test
+# 设置项目名称和目标文件夹
+PROJECT_NAME="test"
+EXECUTABLE="${PROJECT_NAME}"
+
+# 查找所有的.cpp和.h文件
+CPP_FILES=$(find . -type f -name "*.cpp")
+HEADER_FILES=$(find . -type f -name "*.h")
+
+echo ${CPP_FILES}
+
+# -lspdlog -lgtest -lgmock -lssl -lcrypto
+g++ -std=c++23 -O3 -g ${CPP_FILES} -o test && ./test
