21f34ce0dded06fa80355123aa371d4c3c0f4782
terra <544708735@qq.com>
2025-02-12 17:45:18 +0800

    git notes

--- code/test.cpp
+++ code/test.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
-#include <set>
 
-int main() {
-
-    std::set<std::string> set_mm;
-    set_mm.insert("DISTRIBUTE_LOCK:HttpAgentNEV:tokenthread");
-
-    std::cout << set_mm.size() << "\n";
-
-    auto iter = set_mm.find("DISTRIBUTE_LOCK:HttpAgentNEV:tokenthread");
-    if(iter != set_mm.end())
-    {
-        std::cout << "earse \n";
-        set_mm.erase(iter);
-    }
-
-    std::cout << set_mm.size() << "\n";
-
-
-
-    return 0;
+int main()
+{
+  std::cout << "this is print test. \n";
+  return 0;
 }
