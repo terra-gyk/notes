ba6be1a90a5f6f7d2b1aedee9558745a32a53787
terra <544708735@qq.com>
2025-04-23 17:12:18 +0800

    change

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Notes
-
+## 1. 常用的组件
+### 1.1 服务发现
+- Zookeeper
+- Consul 
+### 1.2 消息队列
+- Kafka
+- Rabbitmq
+### 1.3 数据库
+- MySQL
+- PostgreSQL
+- Redis
--- read/rsync_file.sh
+++ read/rsync_file.sh
@@ -1,2 +1,4 @@
-rsync -avz --progress /mnt/d/1_server_down/0_微服务架构设计模式/ ./0_微服务架构设计模式/
-rsync -avz --progress /mnt/d/1_server_down/1_消息队列/ ./1_消息队列/
+rsync -avz --progress --checksum /mnt/d/1_server_down/0_微服务架构设计模式/ ./0_微服务架构设计模式/
+rsync -avz --progress --checksum /mnt/d/1_server_down/1_消息队列/ ./1_消息队列/
+
+find ./ -type f \( -name "*.py" -o -name "*.md" -o -name "*.png" \) -exec chmod 644 {} \;
