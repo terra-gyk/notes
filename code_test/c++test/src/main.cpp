d04ed9d939a90abf6c4ea1dded20ed93f571e0de
terra <544708735@qq.com>
2025-04-28 14:10:12 +0800

    some bash

--- code_test/c++test/src/main.cpp
+++ code_test/c++test/src/main.cpp
@@ -1,15 +1,19 @@
 #include <spdlog/spdlog.h>
+#include <asio.hpp>
 
 #include <string>
 #include <map>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   std::map<int,std::string> param;
-  for(int i = 1; i < argc; i++){
+  for(int i = 1; i < argc; i++)
+  {
     param.emplace(i,argv[i]);
   }
 
-  for( auto node : param ){
+  for( auto node : param )
+  {
     SPDLOG_INFO(node.second);
   }
   // SPDLOG_INFO(param[1]);
