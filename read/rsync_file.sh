d3772cb85722b133b6c8e18e16f8c31cf0e9889b
terra <544708735@qq.com>
2025-09-08 14:44:59 +0800

    remove some

--- read/rsync_file.sh
+++
@@ -1,4 +0,0 @@
-rsync -avz --progress --checksum /mnt/d/1_server_down/0_微服务架构设计模式/ ./0_微服务架构设计模式/
-rsync -avz --progress --checksum /mnt/d/1_server_down/1_消息队列/ ./1_消息队列/
-
-find ./ -type f \( -name "*.py" -o -name "*.md" -o -name "*.png" \) -exec chmod 644 {} \;
